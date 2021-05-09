dd0cdb4f2c921f1664f84ab3b1d9eb9ddb042036
pietrolubello <pietrolubello@gmail.com>
2021-05-09 17:29:15 +0200

    single subroutine for writing output data, solver maintenance

--- src/main.f90
+++ src/main.f90
@@ -4,40 +4,18 @@
 ! ================================================================
 ! Pietro Lubello, Carlo Carcasci
 
-program MultiEnSyst
-
-      USE MODbatterytest
-      USE MODwhichtechs, ONLY: whichtechs
+program MESS2021
 
 
       USE MODcoord, ONLY: icase
-      USE MODparam, ONLY: NdayYear,NhourYear,Maxloc,Nyears,Nstep,MaxGiorni,MaxAddPar
-      USE MODglobalparam, ONLY: RefYear
-
+      USE MODparam, ONLY: NhourYear,Nstep,Maxloc,MaxGiorni
 
       USE MODAddHourlyData, ONLY: AddHourlyData
-
-      USE MODbattery,     ONLY: TempArrBattDeg,SOHtemp,SOHtempbis,totalccal,totalccyc,iswitch !<<< WIP: temporary, to be removed
-      USE MODelectrolyzer, ONLY: iswitchelectrolyzer
-      USE MODfuelcell,     ONLY: iswitchfuelcell
-
-
-      USE MODAggr,     ONLY: Nloc
-      USE MODlocation, ONLY: Nelem
+      USE MODbattery,       ONLY: iswitch             !<<< WIP: temporary, to be removed
+      USE MODelectrolyzer,  ONLY: iswitchelectrolyzer !<<< WIP: temporary, to be removed
+      USE MODfuelcell,      ONLY: iswitchfuelcell     !<<< WIP: temporary, to be removed
 
       implicit real(8) (a-h,o-z), integer(i-n)
-
-
-      real(8), dimension(0:MaxGiorni):: curveNPV0, curveNPV, deltaNPV
-      real(8)                        :: ActPBP,ProfInd
-
-      real(8), dimension(NhourYear)  :: GasConsumpAggr , &
-                                        GasConsumpAggr0
-
-      real(8), dimension(NhourYear)  :: EnBoughtOutAggr ,EnSoldOutAggr , &
-                                        EnBoughtOutAggr0,EnSoldOutAggr0
-
-      type(Battery) :: batteria1
 
       real(8), dimension(Maxloc,NhourYear) :: deltaEEelectr,sourceEEelectr,sinkEEelectr
       real(8), dimension(Maxloc,NhourYear) :: deltaEEheatAmb,sourceEEheatAmb,sinkEEheatAmb
@@ -54,15 +32,16 @@ program MultiEnSyst
       real(8) :: deltaEEheatWatAggrYear,sourceEEheatWatAggrYear,sinkEEheatWatAggrYear  
       real(8) :: deltaEEcoolAmbAggrYear,sourceEEcoolAmbAggrYear,sinkEEcoolAmbAggrYear 
 
-      character(len=19) :: timestamp
-      character(len=19) :: temp 
+      real(8)                        :: ActPBP,ProfInd
+      real(8), dimension(0:MaxGiorni):: curveNPV0, curveNPV, deltaNPV
 
+      real(8), dimension(NhourYear)  :: EnBoughtOutAggr ,EnSoldOutAggr ,GasConsumpAggr, &
+                                        EnBoughtOutAggr0,EnSoldOutAggr0,GasConsumpAggr0
 
 
       ! Program version
-      ! ---------------
 
-      character( 9), parameter ::Version= &                       ! Version
+      character( 9), parameter ::Version= &     
               !'0.00.00.a'   ! First 2021 version            15/02/2021 PL
                '0.01.00.a'   ! Added new components          31/03/2021 PL
                !-.--.--.-|<-Formatted till here
@@ -70,18 +49,7 @@ program MultiEnSyst
       character(10), parameter ::DateVer= '31/03/2021'   ! Release date
                                           !dd/mm/yyyy|<-Formatted till here
 
-
-
-      call CPU_TIME(t1)
-
-      iswitch = 0             ! <<< WIP: necessary for battery ageing
-      iswitchelectrolyzer = 0 ! <<< WIP: necessary for electrolyzer
-      iswitchfuelcell     = 0 ! <<< WIP: necessary for fuelcell
-      AddHourlyData = 0.d0    ! <<< WIP: Initialization of AddHourlyData, should be moved
-
-
       ! Machine Precision
-      ! -----------------
 
       Ieps =Precision(1.e0)
       IepsD=Precision(1.d0)
@@ -90,15 +58,12 @@ program MultiEnSyst
       Eps8Mac=10.d0**(-IepsD)
 
 
-      ! Opening of some writing and reading files
-      ! -----------------------------------------
-
-      ! warning.out file
+      ! Initializing warning.out file
 
       open(unit=8,file='warning.out',form='formatted',status='UNKNOWN')
       rewind 8
 
-      ! Writing version, machine precision etc.
+      ! Writing version and machine precision
 
       write( *,9000)Version,DateVer
       write( 8,9000)Version,DateVer
@@ -114,16 +79,22 @@ program MultiEnSyst
              '                    Double-> EpsD= 10^-',i2.2,/ )
 
 
-      ! Pre-process
-      ! Data read and stored in corresponding modules
-      ! ---------------------------------------------
+      call CPU_TIME(t1)
+
+      ! 1) Pre-process
+      !    Reading and storing input data
+      !    ------------------------------
 
       call ReadInputs()
-      
 
 
-      ! Calling outern layer: aggregate
-      ! --------------------------
+      ! 2) Solving
+      !    -------
+
+      iswitch = 0             ! <<< WIP: necessary for battery ageing
+      iswitchelectrolyzer = 0 ! <<< WIP: necessary for electrolyzer
+      iswitchfuelcell     = 0 ! <<< WIP: necessary for fuelcell
+      AddHourlyData = 0.d0    ! <<< WIP: Initialization of AddHourlyData, should be moved
 
       !VAN0 = 0.d0 ! se vuoi mettere icase =1,1
       !VAN  = 0.d0 ! se vuoi mettere icase =0,0
@@ -149,246 +120,56 @@ program MultiEnSyst
 
                 curveNPV,               & !(O) NPV curve (€)
                 TotActInvest,           & !(O) Total actualized investment
-                EnBoughtOutAggr,        &
-                EnSoldOutAggr,          &
-
-                GasConsumpAggr)
+                EnBoughtOutAggr,EnSoldOutAggr,GasConsumpAggr)
 
 
-        ! Saving VAN in the reference case
+        ! Saving NPV curve in the reference case
 
         select case(icase)
         case(0)
-          curveNPV0(0:Nstep)      = curveNPV(0:Nstep)
-
-          GasConsumpAggr0         = GasConsumpAggr
-
-          EnBoughtOutAggr0        = EnBoughtOutAggr
-          EnSoldOutAggr0          = EnSoldOutAggr
-
+          curveNPV0(0:Nstep) = curveNPV(0:Nstep)
+          EnBoughtOutAggr0   = EnBoughtOutAggr
+          EnSoldOutAggr0     = EnSoldOutAggr
+          GasConsumpAggr0    = GasConsumpAggr
         end select
 
       enddo ! icase
 
 
-      ! VAN analysis
-      ! -----------
+      ! 3) Economic analysis
+      !    -----------------
 
-      call NPVcalc(curveNPV0,    & ! (I) VAN buying from grid
-                   curveNPV,     & ! (I) VAN considered configuration
+      call NPVcalc(curveNPV0,    & ! (I) NPV case 0
+                   curveNPV,     & ! (I) NPV case 1
                    TotActInvest, & ! (I) Total actualized investment
-                   deltaNPV,     & ! (O) VAN difference between two cases
+                   deltaNPV,     & ! (O) VAN difference between case 1 and case 0
                    ActPBP,       & ! (O) Actualized payback period
                    ProfInd)        ! (O) Profit index
 
 
-      ! Writing output files
-      ! --------------------
+      ! 4) Post-process
+      !    Exporting output data   
+      !    ---------------------
 
-      ! 1) NPV analysis
-
-      open(unit=95,file='NPVanalysis.out',form='formatted',status='UNKNOWN')
-      rewind 95
-      write(95,9010),ActPBP
-9010  format(f12.4)!,t15,': Actualizazed PBP (Payback period)')
-      write(95,9011),ProfInd
-9011  format(f12.4)!,t15,': PI (Profit Index)')
-      write(95,9012), deltaNPV(3649)
-9012  format(f12.4)!,t15,': NPV(10)')
-      write(95,9014), deltaNPV(7299)
-9014  format(f12.4)!,t15,': NPV(20)')
-      write(95,9015), deltaNPV(10949)
-9015  format(f12.4)!,t15,': NPV(30)')
-
-
-      open(unit=96,file='NPVcompletedata.out',form='formatted',status='UNKNOWN')
-      rewind 96
-      do i=0,Nstep
-        write(96,*),curveNPV0(i),curveNPV(i),deltaNPV(i)
-      enddo
-9003  format(3(f15.4,2x))
-
-
-      open(unit=99,file='NPVcompletedata.csv',form='formatted',status='UNKNOWN')
-      rewind 99
-      do i=0,Nstep
-        write(99,9008),curveNPV0(i),curveNPV(i),deltaNPV(i)
-      enddo
-9008  format(2(f15.4,','),f12.4)
-
-
-      ! 2) Reference year energy balances
-
-      open(unit=97,file='output_case0.out',form='formatted',status='UNKNOWN')
-      rewind 97
-      do i=1,NhourYear
-        write(97,9004),EnBoughtOutAggr0(i),EnSoldOutAggr0(i), &  
-                       GasConsumpAggr0(i)
-      enddo
-9004  format(3(f12.4,2x))
-
-      open(unit=98,file='output_case1.out',form='formatted',status='UNKNOWN')
-      rewind 98
-      do i=1,NhourYear
-        write(98,9005),EnBoughtOutAggr (i),EnSoldOutAggr (i), &  
-                       GasConsumpAggr (i)
-      enddo
-9005  format(3(f12.4,2x))
-
-
-      ! 3) Battery degradation
-
-      ! >>> TEMP: verifying how battery degradation works <<<
-      open(unit=89,file='batterydegr.out',form='formatted',status='unknown')
-      rewind 89
-      do i=1,13
-        write(89,9023),TempArrBattDeg(i)
-      enddo
-9023  format(f12.4)
-      ! >>> TEMP: verifying how battery degradation works <<<
-
-
-      ! >>> TEMP: grafici battery degradation <<<
-      open(unit=88,file='SOHtemp.out',form='formatted',status='unknown')
-      rewind 88
-      do i=1,1565!NhourYear*Nyears
-        write(88,9024),SOHtempbis(i) !SOHtemp(i)
-      enddo
-9024  format(f12.4)
-      open(unit=87,file='ageing.out',form='formatted',status='unknown')
-      rewind 87
-      write(87,9025),totalccyc
-      write(87,9025),totalccal
-9025  format(f12.4)
-      ! >>> TEMP: grafici battery degradation<<<
-
-
-
-      ! 4) Detailed infos for techs
-
-      open(unit=81,file='techinfo.csv',form='formatted',status='UNKNOWN')
-      rewind 81
-      write(81,9040,Advance = 'No') 'timestamp;'
-      do ii=1,Nloc
-        do jj = 1,Nelem(ii)
-          do kk = 1,MaxAddPar
-            if (ii.eq.Nloc .and. jj.eq.Nelem(ii) .and. kk.eq.MaxAddPar) then
-              write(81,9041) ii,whichtechs(ii,jj),kk
-            else
-              write(81,9042,Advance = 'No') ii,whichtechs(ii,jj),kk
-            endif
-          enddo
-        enddo
-      enddo
-
-9041  format(i0,a2,i0)
-9042  format(i0,a2,i0,';')
-
-      do ll = 1,NhourYear
-        call hourtodate(RefYear,ll,timestamp)
-        write(81,9070,Advance = 'No') timestamp
-        do ii = 1,Nloc
-          do jj = 1,Nelem(ii)
-            do kk = 1,MaxAddPar
-              if (ii.eq.Nloc .and. jj.eq.Nelem(ii) .and. kk.eq.MaxAddPar) then
-                write(81,9043) AddHourlyData(ii,jj,kk,ll)
-              else
-                write(81,9044,Advance = 'No') AddHourlyData(ii,jj,kk,ll)
-              endif
-            enddo
-          enddo
-        enddo
-      enddo
-
-9043  format(f0.3)
-9044  format(f0.3,';')
-        
-
-      ! 5) location balances
-
-      open(unit=82,file='locationsenergybalances.csv',form='formatted',status='UNKNOWN')
-      rewind 82
-      write(82,9040,Advance = 'No') 'timestamp;'
-      do ii = 1,Nloc
-        if (ii.eq.Nloc) then
-          write(82,9045) ii,ii,ii,ii,ii,ii,ii,ii,ii,ii,ii,ii
-        else
-          write(82,9046,Advance = 'No') ii,ii,ii,ii,ii,ii,ii,ii,ii,ii,ii,ii
-        endif
-      enddo
-
-      do ii = 1,NhourYear
-        call hourtodate(RefYear,ii,timestamp)
-        write(82,9070,Advance = 'No') timestamp
-        do jj = 1,Nloc
-          if (jj.eq.Nloc) then
-            write(82, 9047) deltaEEelectr(jj,ii),sourceEEelectr(jj,ii),sinkEEelectr(jj,ii),    &   
-                            deltaEEheatAmb(jj,ii),sourceEEheatAmb(jj,ii),sinkEEheatAmb(jj,ii), &
-                            deltaEEheatWat(jj,ii),sourceEEheatWat(jj,ii),sinkEEheatWat(jj,ii), &
-                            deltaEEcoolAmb(jj,ii),sourceEEcoolAmb(jj,ii),sinkEEcoolAmb(jj,ii)
-          else
-            write(82, 9048, Advance = 'No') deltaEEelectr(jj,ii),sourceEEelectr(jj,ii),sinkEEelectr(jj,ii),    &   
-                                            deltaEEheatAmb(jj,ii),sourceEEheatAmb(jj,ii),sinkEEheatAmb(jj,ii), &
-                                            deltaEEheatWat(jj,ii),sourceEEheatWat(jj,ii),sinkEEheatWat(jj,ii), &
-                                            deltaEEcoolAmb(jj,ii),sourceEEcoolAmb(jj,ii),sinkEEcoolAmb(jj,ii)
-          endif
-        enddo
-      enddo
-
-9045  format(i0'_EnBalanceElectr;',i0'_EnProdElectr;',i0'_EnConsElectr;',    & 
-             i0'_EnBalanceAmbHeat;',i0'_EnProdAmbHeat;',i0'_EnConsAmbHeat;', &
-             i0'_EnBalanceWatHeat;',i0'_EnProdWatHeat;',i0'_EnConsWatHeat;', &
-             i0'_EnBalanceAmbCool;',i0'_EnProdAmbCool;',i0'_EnConsAmbCool')
-9046  format(i0'_EnBalanceElectr;',i0'_EnProdElectr;',i0'_EnConsElectr;',    & 
-             i0'_EnBalanceAmbHeat;',i0'_EnProdAmbHeat;',i0'_EnConsAmbHeat;', &
-             i0'_EnBalanceWatHeat;',i0'_EnProdWatHeat;',i0'_EnConsWatHeat;', &
-             i0'_EnBalanceAmbCool;',i0'_EnProdAmbCool;',i0'_EnConsAmbCool;')
-9047  format(11(f12.3,';'),f12.3)
-9048  format(12(f12.3,';'))
-
-
-      ! 6) aggregate balances
-
-      open(unit=83,file='cityenergybalances.csv',form='formatted',status='UNKNOWN')
-      rewind 83
-      write(83,9040,Advance = 'No') 'timestamp;'
-      write(83,9051) 'EnBalanceElectr;' ,'EnProdElectr;' ,'EnConsElectr;' , &      
-                     'EnBalanceAmbHeat;','EnProdAmbHeat;','EnConsAmbHeat;', &
-                     'EnBalanceWatHeat;','EnProdWatHeat;','EnConsWatHeat;', &
-                     'EnBalanceAmbCool;','EnProdAmbCool;','EnConsAmbCool'
-
-      do ii = 1,NhourYear
-        call hourtodate(RefYear,ii,timestamp)
-        write(83,9070,Advance = 'No') timestamp
-        write(83, 9052) deltaEEelectrAggr(ii),sourceEEelectrAggr(ii),sinkEEelectrAggr(ii),    &   
-                        deltaEEheatAmbAggr(ii),sourceEEheatAmbAggr(ii),sinkEEheatAmbAggr(ii), &
-                        deltaEEheatWatAggr(ii),sourceEEheatWatAggr(ii),sinkEEheatWatAggr(ii), &
-                        deltaEEcoolAmbAggr(ii),sourceEEcoolAmbAggr(ii),sinkEEcoolAmbAggr(ii)
-      enddo
-
-9051  format(12a)
-9052  format(11(f12.3,';'),f12.3)
-
-
-9040  format(a10)
-9070  format(a19,';')
-
-
-
-
-!      efficiency = 0.99d0
-!      SoC = 0.d0
-!      batteria1 = Battery(efficiency,SoC)
-!      write(*,*) batteria1%efficiency, batteria1%SoC
-!      call aggiornamentoSoC(batteria1)
-!      write(*,*) batteria1%efficiency, batteria1%SoC
+      call WriteOutputs(ActPBP,ProfInd,curveNPV0,curveNPV,deltaNPV,      &
+                        EnBoughtOutAggr0,EnSoldOutAggr0,GasConsumpAggr0, &
+                        EnBoughtOutAggr ,EnSoldOutAggr ,GasConsumpAggr , &                 
+                        deltaEEelectr,sourceEEelectr,sinkEEelectr,       &
+                        deltaEEheatAmb,sourceEEheatAmb,sinkEEheatAmb,    &
+                        deltaEEheatWat,sourceEEheatWat,sinkEEheatWat,    &
+                        deltaEEcoolAmb,sourceEEcoolAmb,sinkEEcoolAmb,    &
+                        deltaEEelectrAggr,sourceEEelectrAggr,sinkEEelectrAggr,    &
+                        deltaEEheatAmbAggr,sourceEEheatAmbAggr,sinkEEheatAmbAggr, &
+                        deltaEEheatWatAggr,sourceEEheatWatAggr,sinkEEheatWatAggr, &
+                        deltaEEcoolAmbAggr,sourceEEcoolAmbAggr,sinkEEcoolAmbAggr)
  
 
       call CPU_TIME(t2)
+
       write(*,9006),t2-t1
 9006  format(' Time of operation was',f8.4,' seconds')
 
 
       stop ' Apparently everything went well!'
 
-end program MultiEnSyst
+end program MESS2021
